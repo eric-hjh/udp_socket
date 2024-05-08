92622b63aa90abde798af0b6f9da9f837937b2c8
hwang <hgh1057@gmail.com>
2024-05-09 03:25:14 +0900

    UDP 연결

--- udp_server.c
+++ udp_server.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFSIZE 30
+#define BUFSIZE 1024
 void error_handling(char *message) {
     perror(message);
     exit(1);
@@ -25,37 +25,44 @@ int main (int argc, char **argv) {
   struct sockaddr_in clnt_addr; 
   socklen_t clnt_addr_size;
 
+  // 실행할 때 입력값 검사
   if(argc!=2){
     printf("Usage: %s <port>\n", argv[0]);
     exit (1);
   }
 
-  serv_sock = socket (PF_INET, SOCK_DGRAM, 0) ; // 성공하면 파일 디스크립터, 실패하면 -1 
-
-  if(serv_sock == -1)
+  serv_sock = socket(AF_INET, SOCK_DGRAM, 0); // 성공하면 파일 디스크립터, 실패하면 -1 
+  if(serv_sock == -1) 
     error_handling("socket() error");
 
-  memset(&serv_addr, 0, sizeof (serv_addr)) ; // 메모리 블록을 특정 값으로 설정하는 함수, 쓰레기값이 남아있을 수 있으니 초기화를 해야한다
+  memset(&serv_addr, 0, sizeof (serv_addr)); // 메모리 블록을 특정 값으로 설정하는 함수, 쓰레기값이 남아있을 수 있으니 초기화를 해야한다
 
-  // ?
   serv_addr.sin_family = AF_INET;
-  serv_addr. sin_addr.s_addr = htonl(INADDR_ANY) ;
-  serv_addr.sin_port = htons(atoi(argv [1])) ;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+  serv_addr.sin_port = htons(atoi(argv[1])); // atoi : 문자열 -> 정수
 
   
-  if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof (serv_addr)) == -1)
+  if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))
     error_handling("bind() error");
+
+  printf("waiting for messages\n");
 
   sleep (5) ;
 
   while (1){
     clnt_addr_size = sizeof(clnt_addr);
-    sleep (1) ;
+    sleep (1);
     str_len = recvfrom(serv_sock, message, BUFSIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size) ;
+    if(str_len < 0){ 
+      error_handling("recvfrom() error "); 
+    }
+    message[str_len] = '\0';
 
-    printf("수신 번호: %d 1n", num++) ;
-    sendto (serv_sock, message, str_len, 0,
-      (struct sockaddr*)&clnt_addr, sizeof (clnt_addr)) ;
+    printf("ip : %s\n", inet_ntoa(clnt_addr.sin_addr));
+    printf("received data : %s\n\n", message);
+ 
+    sendto(serv_sock, message, strlen(message), 0, (struct sockaddr*)&clnt_addr, sizeof (clnt_addr)) ;
   }
+  close(serv_sock);
   return 0;
 }
